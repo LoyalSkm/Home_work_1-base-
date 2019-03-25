34bebee82d449e975f974f9f4618fcacc0192de6
Lol <konstienzp@gmail.com>
2019-03-25 12:06:13 +0200

    proverka na poluperimetr

--- 1st.py
+++ 1st.py
@@ -1,12 +1,22 @@
-import sys
+import sys, math
+
 A = float(input("Enter side A of triangle: "))
 B = float(input("Enter side B: "))
 C = float(input("Enter side C: "))
-if (A == 0) or (B == 0) or (C == 0):
+t = [A, B, C] #Список сторон треугольника
+
+
+Big_side = max(t) # Из не равенства треугольника длина
+                # наибольшей стороны додлжна быть меньшей
+                # либо равной сумме 2х остальных.
+t.remove(Big_side)
+Sum = sum(t)
+
+if Big_side <= Sum:
+    p = eval("1/2*(A+B+C)")
+    sq = eval("(p*(p-A)*(p-B)*(p-C))**0.5")  # формула Герона
+    print("Perimetr is: " + str(A + B + C))
+    print("Square is: " + str(sq))
+else:
     print("Не правильный какойто у вас тут триугольник*(")
     sys.exit()
-p = eval("1/2*(A+B+C)")                  # разность полупериметра
-sq = eval("(p*(p-A)*(p-B)*(p-C))**0.5")  # формула Герона
-print("Perimetr is: " + str(A+B+C))
-print("Square is: " + str(sq))
-
