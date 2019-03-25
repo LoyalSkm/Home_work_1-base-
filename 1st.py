4dbc00b9dd34331ca3b90991840db5688af32453
Lol <konstienzp@gmail.com>
2019-03-25 14:30:19 +0200

    pravka 1st and 5th

--- 1st.py
+++ 1st.py
@@ -5,14 +5,22 @@ B = float(input("Enter side B: "))
 C = float(input("Enter side C: "))
 t = [A, B, C] #Список сторон треугольника
 
+for i in t:
+    if i<0:
+        print("начение не может быть отрицательным")
+        sys.exit()
+
 
 Big_side = max(t) # Из не равенства треугольника длина
                 # наибольшей стороны додлжна быть меньшей
                 # либо равной сумме 2х остальных.
 t.remove(Big_side)
-Sum = sum(t)
 
-if Big_side <= Sum:
+Sum1 = math.fabs(t[0])
+Sum2 = math.fabs(t[1])
+SUM = Sum1 + Sum2
+
+if math.fabs(Big_side) <= SUM:
     p = eval("1/2*(A+B+C)")
     sq = eval("(p*(p-A)*(p-B)*(p-C))**0.5")  # формула Герона
     print("Perimetr is: " + str(A + B + C))
